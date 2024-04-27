a1bae554483e70a0f4e14488ed8d090cc0802a20
Yoanvari <yoanvari123@gmail.com>
2024-04-28 00:38:58 +0700

    Halaman Login Selesai

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,36 +4,45 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&display=swap">
 </head>
 <body>
-  <main class="h-screen grid place-content-center" style="background-color: rgb(173, 217, 138)">
-    <div class="border rounded-3xl shadow-xl" style="background-color: white">
-      <aside class="p-12 flex flex-row">
-        <div class="flex flex-col gap-4">
-          <div class="">
-            SIWALANG
-          </div>
-          <section>
-            <label for="username" class="block text-sm font-medium leading-none text-gray-900">NIK</label>
-              <div class="mt-2">
-                <input id="username" name="username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 bg-gray-300">
-              </div>
-          </section>
-          <section>
-            <label for="password" class="block text-sm font-medium leading-none text-gray-900">Password</label>
-              <div class="mt-2">
-                <input id="password" name="password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 bg-gray-300">
-              </div>
-          </section>
-          <div class="">
-            <button type="submit" class="flex w-full justify-center rounded-md bg-lime-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-lime-400">LOG IN</button>
+  <div class="h-screen flex flex-wrap justify-center content-center" style="background-color: rgb(173, 217, 138)">
+    <div class="rounded-3xl grid grid-cols-3 " style="background-color: white; box-shadow: rgba(14, 30, 37, 0.12) 0px 0px 0px 0px, rgba(14, 30, 37, 0.32) 0px 0px 20px 0px;">
+      
+      <div class="p-10 col-span-2 flex flex-col gap-4">
+        <div class="flex flex-row text-3xl font-black" style="font-family: Inter">
+          <p style="color: rgb(65, 184, 90)">
+            SI
+          </p>
+          <p style="color: rgb(0, 86, 47)">
+            WALANG
+          </p>
+        </div>
+        <div>
+          <label for="username" class="block text-sm font-bold leading-none text-gray-600">NIK</label>
+          <div class="mt-2">
+            <input id="username" name="username" type="text" placeholder="Masukkan NIK anda" autocomplete="username" required class="w-full rounded-md border-0 py-1 px-4 focus:ring-2 focus:ring-lime-400 focus:outline-none text-gray-900 bg-gray-300">
           </div>
         </div>
-        <div class="">
-          <img src="{{ asset('img/logo-lang2.png') }}" alt="lang-lang" class="scale-75">
+        <div>
+          <label for="password" class="block text-sm font-bold leading-none text-gray-600">Password</label>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" placeholder="Masukkan password anda" required class="w-full rounded-md border-0 py-1 px-4 focus:ring-2 focus:ring-lime-400 focus:outline-none text-gray-900 bg-gray-300">
+          </div>
         </div>
-      </aside>
+        <div class="pt-3">
+          <button type="submit" class="flex w-full justify-center rounded-md bg-lime-300 px-3 py-1.5 text-sm font-bold leading-6 text-black shadow-sm hover:bg-lime-400" style="font-family: Inter">LOG IN</button>
+        </div>
+      </div>
+
+      <div class="flex flex-col justify-center" style="background-color: rgb(173, 217, 138); border-radius: 100% 42px 42px 100%">
+        <div>
+          <img src="{{ asset('img/logo-lang2.png') }}" alt="lang-lang" class="scale-50">
+        </div>
+      </div>
+
     </div>
-  </main>
+  </div>
 </body>
 </html>
