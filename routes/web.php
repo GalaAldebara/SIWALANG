40872ce0de9f45d18d0b2b7d020edf8608b8801e
Yoanvari <yoanvari123@gmail.com>
2024-04-30 14:44:51 +0700

    merge route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pengaduan/form', function () {
+    return view('warga.pengaduan.PengaduanForm');
+});
+
+Route::get('/pengaduan/index', function () {
+    return view('warga.pengaduan.index');
+});
