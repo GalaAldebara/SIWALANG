b059d50c94dc51ad0a7058b0c4b34b45152db182
Yoanvari <yoanvari123@gmail.com>
2024-04-30 14:40:00 +0700

    warga pengaduan index

---
+++ resources/views/warga/pengaduan/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&display=swap">
+</head>
+<body>
+    <div class="container bg-black">
+        <div class="">
+            <img src="{{ asset('img/desa-lang2.png') }}" alt="desa-langlang" class="object-cover w-full max-h-80">
+        </div>
+    </div>
+</body>
+</html>
