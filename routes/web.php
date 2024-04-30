9d3956a1dea25a8c91add075b30534d703d9606b
GalaAldebara <lalasipo20@gmail.com>
2024-04-30 14:35:31 +0700

    Halaman Pengaduan Form

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pengaduan', function () {
+    return view('warga.Pengaduan.PengaduanForm');
+});
