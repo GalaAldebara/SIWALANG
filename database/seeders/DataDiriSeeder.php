e6b58b6325d66347ee585af8e9440c4c05136989
Yoanvari <yoanvari123@gmail.com>
2024-06-10 17:26:46 +0700

    update seeder

--- database/seeders/DataDiriSeeder.php
+++ database/seeders/DataDiriSeeder.php
@@ -15,51 +15,87 @@ class DataDiriSeeder extends Seeder
     public function run(): void
     {
         $faker = Faker::create('id_ID');
-        $initialNik = 357305006;
+        $initialNik = 3576014403940001;
         $fotoPath = 'pexels_andrea_piacquadio_874158_grid_300_x_300_jpg_1.png';
+        $fotoKtp = 'Gambar KK.jpg';
+        $fotoKk = 'image 4.png';
+        $fotoSN = 'surat-nikah.jpeg';
 
         $data = [];
+        $kkCounter = 0;
+        $nullCounter = 0;
+        $kkNumber = 3172010502090900;
+        $hubungan_kk_sequence = ['kepala keluarga', 'isteri', 'anak'];
 
-        foreach (range(0, 44) as $index) {
-            $nik = strval($initialNik + $index);
-            $rt = 1; // default value
+        foreach (range(0, 299) as $index) {
+            // Generate NIK
+            do {
+                $nik = strval($initialNik + $index);
+                $existingNik = DB::table('data_diri')->where('nik', $nik)->exists();
+            } while ($existingNik);
 
-            if ($nik >= 357305009 && $nik <= 357305018) {
+            $rt = 0;
+
+            if ($nik >= 3576014403940001 && $nik <= 3576014403940060) {
+                $rt = 1;
+            } elseif ($nik >= 3576014403940061 && $nik <= 3576014403940120) {
                 $rt = 2;
-            } elseif ($nik >= 357305019 && $nik <= 357305028) {
+            } elseif ($nik >= 3576014403940121 && $nik <= 3576014403940180) {
                 $rt = 3;
-            } elseif ($nik >= 357305029 && $nik <= 357305038) {
+            } elseif ($nik >= 3576014403940181 && $nik <= 3576014403940240) {
                 $rt = 4;
-            } elseif ($nik >= 357305039 && $nik <= 357305044) {
+            } elseif ($nik >= 3576014403940241 && $nik <= 3576014403940300) {
                 $rt = 5;
             }
 
+            $nullCounter++;
+            $kkCounter++;
+            $hubungan_kk = $hubungan_kk_sequence[$kkCounter % count($hubungan_kk_sequence)];
+
+            // Update nomor KK setiap 3 inputan
+            if ($index % 3 == 0) {
+                $kkNumber++; // Menambah nomor KK
+            }
+
+            // Set foto_surat_nikah menjadi null setelah 2 kali input
+            if ($nullCounter < 2) {
+                $foto_surat_nikah = $fotoSN;
+            } else {
+                $foto_surat_nikah = null;
+            }
+
             $data[] = [
-                'nik' => $nik,
+                'nik' => (string)$nik,
                 'tempat_lahir' => $faker->city,
                 'tanggal_lahir' => $faker->date('Y-m-d', '2005-01-01'),
                 'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
                 'status_perkawinan' => $faker->randomElement(['Nikah', 'Belum Nikah']),
                 'no_telp' => $faker->numerify('08##########'),
-                'no_kk' => $faker->numerify('################'),
+                'no_kk' => (string)$kkNumber,
                 'hubungan_kk' => $faker->randomElement(['kepala keluarga', 'isteri', 'anak']),
                 'status_kependudukan' => $faker->randomElement(['Warga Asli', 'Kontrak', 'Kost', 'Pindahan']),
                 'kewarganegaraan' => 'Indonesia',
-                'pekerjaan' => $faker->jobTitle,
-                'alamat_tinggal' => $faker->address,
+                'pekerjaan' => $faker->randomElement(['swasta', 'wiraswasta', 'petani', 'buruh', 'tidak_bekerja']),
+                'alamat_tinggal' => $faker->streetAddress(),
                 'rt' => $rt,
                 'rw' => 1,
-                'kelurahan' => $faker->city,
-                'kecamatan' => $faker->city,
-                'kota' => $faker->city,
-                'alamat_ktp' => $faker->address,
-                'foto_ktp' => $fotoPath,
-                'foto_kk' => $fotoPath,
-                'foto_surat_nikah' => $fotoPath,
+                'kelurahan' => 'Lang-Lang',
+                'kecamatan' => 'Singosari',
+                'kota' => 'Malang',
+                'alamat_ktp' => ($faker->randomElement(['Warga Asli', 'Kontrak', 'Kost', 'Pindahan']) == 'Warga Asli') ?
+                    $faker->streetAddress() . ' RT ' . $rt . ' RW 1 Lang-Lang Singosari Malang' : $faker->address,
+                'foto_ktp' => $fotoKtp,
+                'foto_kk' => $fotoKk,
+                'foto_surat_nikah' => $foto_surat_nikah,
                 'foto_profil' => $fotoPath,
                 'created_at' => now(),
                 'updated_at' => now(),
             ];
+
+            $nullCounter++;
+            if ($nullCounter >= 2) {
+                $nullCounter = 0;
+            }
         }
 
         DB::table('data_diri')->insert($data);
